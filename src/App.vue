5cc653ebc5a1fa2c2a11de993a39a7e0bebdada2
jamilzzabottoneto <jamilzabneto@hotmail.com>
2021-11-01 20:18:09 -0300

    Primeiras Alterações no Projeto

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
 <template>
-  <div>
-    <h1>Iniciando o projeto</h1>
-    <h2>Calculadora em VueJs</h2>
+  <div id="app">
+      <h1>Hello Vue.JS</h1>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+
+}
 </script>
 
 <style>
+@font-face {
+    font-family: "RobotoMono";
+    src: url("./fonts/RobotoMono-Thin.ttf");
+}
+
+*{
+    font-family: "RobotoMono", "monospace";
+}
+
+body{
+    margin: 0;
+}
+
+#app{
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    color: #fff;
+
+    background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46,73));
+}
+
 </style>
