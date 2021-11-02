581990f2d5d7c197961255c4f1889a66e0befadd
jamilzzabottoneto <jamilzabneto@hotmail.com>
2021-11-02 19:21:46 -0300

    Implementando a logica dos metodos e finalizando a calculadora

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,51 +1,56 @@
 <template>
-  <button class="button" :class="{double, triple, operation}">{{ label }}</button>
+  <button class="button"
+    :class="{ double, triple, operation }"
+    @click="$emit('onClick', label)"
+  >
+    {{ label }}
+  </button>
 </template>
 
 <script>
 export default {
-    //propriedades do componente
+  //propriedades do componente
   props: {
-      label: {},
-      operation: {type: Boolean},
-      double: {type: Boolean},
-      triple: {type: Boolean},
+    label: {},
+    operation: { type: Boolean },
+    double: { type: Boolean },
+    triple: { type: Boolean },
   },
 };
 </script>
 
 <style>
-:root{
-    --bg-button: #f0f0f0;
-    --border-button: solid 1px #888;
+:root {
+  --bg-button: #f0f0f0;
+  --border-button: solid 1px #888;
 }
-.button{
-    font-size: 1.4em;
-    background-color: var(--bg-button);
-    border: none;
-    border-right: var(--border-button);
-    border-bottom: var(--border-button);
-    outline: none;
-}
-
-.button:active{
-    background-color: #ccc;
+.button {
+  font-size: 1.4em;
+  background-color: var(--bg-button);
+  border: none;
+  border-right: var(--border-button);
+  border-bottom: var(--border-button);
+  outline: none;
 }
 
-.button.double{
-    grid-column: span 2;
+.button:active {
+  background-color: #ccc;
 }
 
-.button.triple{
-    grid-column: span 3;
+.button.double {
+  grid-column: span 2;
 }
 
-.button.operation{
-    background-color: #fa8231;
-    color: #fff;
+.button.triple {
+  grid-column: span 3;
 }
 
-.button.operation:active{
-    background-color: #fa8231cc;
+.button.operation {
+  background-color: #fa8231;
+  color: #fff;
+}
+
+.button.operation:active {
+  background-color: #fa8231cc;
 }
 </style>
--- src/main/Calculadora.vue
+++ src/main/Calculadora.vue
@@ -1,47 +1,105 @@
 <template>
-    <div class="calculator">
-        <Display :value="1000"/>
-        <Button label="AC" triple/>
-        <Button label="/" operation/>
-        <Button label="7" />
-        <Button label="8" />
-        <Button label="9" />
-        <Button label="*" operation/>
-        <Button label="4" />
-        <Button label="5" />
-        <Button label="6" />
-        <Button label="-" operation/>
-        <Button label="1" />
-        <Button label="2" />
-        <Button label="3" />
-        <Button label="+" operation/>
-        <Button label="0" double/>
-        <Button label="." />
-        <Button label="=" operation/>
-    </div>
+  <div class="calculator">
+    <Display :value="displayValue" />
+    <Button label="AC" triple @onCLick="clearMemory" />
+    <Button label="/" operation @onClick="setOperation" />
+    <Button label="7" @onClick="addDigit" />
+    <Button label="8" @onClick="addDigit" />
+    <Button label="9" @onClick="addDigit" />
+    <Button label="*" operation @onClick="setOperation" />
+    <Button label="4" @onClick="addDigit" />
+    <Button label="5" @onClick="addDigit" />
+    <Button label="6" @onClick="addDigit" />
+    <Button label="-" operation @onClick="setOperation" />
+    <Button label="1" @onClick="addDigit" />
+    <Button label="2" @onClick="addDigit" />
+    <Button label="3" @onClick="addDigit" />
+    <Button label="+" operation @onClick="setOperation" />
+    <Button label="0" double @onClick="addDigit" />
+    <Button label="." @onClick="addDigit" />
+    <Button label="=" operation @onClick="setOperation" />
+  </div>
 </template>
 
 <script>
 import Display from "../components/Display.vue";
 import Button from "../components/Button.vue";
 
-//Metodos
-
 export default {
+  data: function () {
+    return {
+      displayValue: "0",
+      clearDisplay: false,
+      operation: null,
+      values: [0, 0],
+      current: 0,
+    };
+  },
+
   //Registrar os components
   components: { Button, Display },
+
+  //Metodos
+  methods: {
+    clearMemory() {
+      Object.assign(this.$data, this.$options.data());
+    },
+    setOperation(operation) {
+      if (this.current === 0) {
+        this.operation = operation;
+        this.current = 1;
+        this.clearDisplay = true;
+      } else {
+        const equals = operation === "=";
+        const curretnOperation = this.operation;
+
+        try {
+          this.values[0] = eval(
+            `${this.values[0]} ${curretnOperation} ${this.values[1]}`
+          );
+        } catch (error) {
+          this.$emit("onError", error);
+        }
+
+        this.values[1] = 0;
+        this.displayValue = this.values[0];
+        this.operation = equals ? null : operation;
+        this.current = equals ? 0 : 1;
+        this.clearDisplay = !equals;
+      }
+    },
+    addDigit(n) {
+      if (n === "." && this.displayValue.includes(".")) {
+        return;
+      }
+
+      const clearDisplay = this.displayValue === "0" || this.clearDisplay;
+      const currentValue = clearDisplay ? "" : this.displayValue;
+      const displayValue = currentValue + n;
+
+      this.displayValue = displayValue;
+      this.clearDisplay = false;
+      this.values[this.current] = displayValue;
+
+    //   if (n !== ".") {
+    //     const i = this.current;
+    //     const newValue = parseFloat(displayValue);
+    //     this.values[i] = newValue;
+    //   }
+    },
+  },
 };
 </script>
 
 <style>
-.calculator{
-    height: 320px;
-    width: 235px;
-    border-radius: 5px;
-    overflow: hidden;
+.calculator {
+  height: 320px;
+  width: 235px;
+  border-radius: 5px;
+  overflow: hidden;
 
-    display: grid;
-    grid-template-columns: repeat(4, 25%);
-    grid-template-rows: 1fr 48px 48px 48px 48px 48px;
+  display: grid;
+  grid-template-columns: repeat(4, 25%);
+  grid-template-rows: 1fr 48px 48px 48px 48px 48px;
 }
 </style>
